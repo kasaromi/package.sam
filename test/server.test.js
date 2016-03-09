ba56dcad781f7190e0c632f3b9abf3486a0996d0
Katherine Bowler <katherine.bowler@gmail.com>
2016-03-09 11:16:28 +0000

    add initial server setup

---
+++ test/server.test.js
@@ -0,0 +1,17 @@
+var tape = require('tape');
+var url = require('url');
+var querystring = require('querystring');
+var server = require('../src/server.js');
+
+tape('/ endpoint redirect to twitter login', function(t) {
+    var options = {
+        url: '/',
+        method:'GET'
+    };
+
+    server.inject(options, function(response) {
+        console.log(response.path);
+        t.ok(response.statusCode, 302, 'server redirects');
+        t.end();
+    });
+});
