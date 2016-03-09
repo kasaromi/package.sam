3c2e6fa19c01906e455c7bf2b3a142e6eba978c3
Katherine Bowler <katherine.bowler@gmail.com>
2016-03-09 16:00:41 +0000

    Add route for getting request token

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 var Hapi = require('hapi');
 var http = require('https');
+var request = require('request');
 // var url = require('url');
 var querystring = require('querystring');
 require('env2')('config.env');
@@ -8,17 +9,44 @@ var server = new Hapi.Server();
 
 var port = process.env.PORT || 3000;
 
-function getRequestToken() {
-    return "https://api.twitter.com/oauth/request_token";
+function getRequestToken(options, cb) {
+    // var request = http.request(options, function(response) {
+    //     var body = '';
+    //     response.on('data', function(chunk) {
+    //         body += chunk;
+    //     });
+    //     response.on('end', function() {
+    //         cb(null, body);
+    //     });
+    // });
+    //
+    // request.on('error', function(err) {
+    //     console.error('request to ' + options.host + ' failed');
+    //     cb(err);
+    // });
+    //
+    // request.write(options.body);
+    // request.end();
 }
 
-function createTwitterAuthRoute(){
+function createTwitterAuthRoute(token){
     console.log(process.env.TWITTER_CLIENT_ID);
-    return "https://api.twitter.com/oauth/authorize?" + querystring.stringify({
-        client_id: process.env.TWITTER_CLIENT_ID,
+    return "https://api.twitter.com/oauth/authenticate?" + querystring.stringify({
+        oauth_token: token,
         redirect_uri: process.env.BASE_URL + "/home"
     });
 }
+
+var requestTokenUrl = "https://api.twitter.com/oauth/request_token";
+var consumerKey = process.env.TWITTER_CLIENT_ID;
+var consumerSecret = process.env.TWITTER_CLIENT_SECRET;
+var oauth = {
+    callback: 'http://localhost:3000/signin-with-twitter',
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret
+};
+var oauthToken = '';
+var oauthTokenSecret = '';
 
 server.connection({
     port: port
@@ -29,8 +57,33 @@ server.route([
         path: '/',
         method: 'GET',
         handler: function(req, reply){
-            reply.redirect(createTwitterAuthRoute());        }
+            request.post({url: requestTokenUrl, oauth: oauth}, function(err, r, body) {
+                var reqData = querystring.parse(body);
+                oauthToken = reqData.oauth_token;
+                oauthTokenSecret = reqData.oauth_token_secret;
+                var uri = 'https://api.twitter.com/oauth/authenticate?' + querystring.stringify({oauth_token: oauthToken});
+                var requestToken = uri.split('=')[1];
+                reply.redirect(createTwitterAuthRoute(requestToken));
+            });
+        }
     }, {
+        path: '/signin-with-twitter',
+        method: 'GET',
+        handler: function(req, reply) {
+            var authReqData = req.query;
+            oauth.token = authReqData.oauth_token;
+            oauth.token_secret = oauthTokenSecret;
+            oauth.verifier = authReqData.oauth_verifier;
+
+            var accessTokenUrl = "https://api.twitter.com/oauth/access_token";
+
+            request.post({url: accessTokenUrl, oauth: oauth}, function(e, r, body) {
+                var authenticatedData = querystring.parse(body);
+                console.log(authenticatedData, '-------------', body);
+            });
+        }
+    },
+    {
         path: '/home',
         method: 'GET',
         handler: function(req, reply){
