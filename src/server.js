bf9813f4e009ee6f4ccdfa5ddb95fc43c29493a5
RobStallion <Robertefrancis18@gmail.com>
2016-03-09 14:15:53 +0000

    start server in index.js and added twitterAuth function in server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,44 @@
 var Hapi = require('hapi');
 var http = require('https');
-var url = require('url');
+// var url = require('url');
+var querystring = require('querystring');
+require('env2')('config.env');
+
 var server = new Hapi.Server();
+
 var port = process.env.PORT || 3000;
 
+function getRequestToken() {
+    return "https://api.twitter.com/oauth/request_token";
+}
+
 function createTwitterAuthRoute(){
-    return 'twitter.com';
+    console.log(process.env.TWITTER_CLIENT_ID);
+    return "https://api.twitter.com/oauth/authorize?" + querystring.stringify({
+        client_id: process.env.TWITTER_CLIENT_ID,
+        redirect_uri: process.env.BASE_URL + "/home"
+    });
 }
 
 server.connection({
     port: port
-})
+});
 
 server.route([
     {
         path: '/',
         method: 'GET',
         handler: function(req, reply){
-            reply.redirect(createTwitterAuthRoute())
+            reply.redirect(createTwitterAuthRoute());
         }
-    }]);
+    }, {
+        path: '/home',
+        method: 'GET',
+        handler: function(req, reply){
+            // var home = __dirname + "/../"
+            reply("hey Mireia");
+        }
+    }
+]);
 
 module.exports = server;
